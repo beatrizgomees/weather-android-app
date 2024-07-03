a6a424b288e24747cadfb7630bf8f20dd38b905e
beatrizgomees <beatrizgomesxx@gmail.com>
2024-07-03 16:03:01 -0300

    Prática 03 - Parte 03 - Adicionando elementos com um Diálogo

--- app/src/main/java/com/github/beatrizgomees/weatherapp/activitys/MainActivity.kt
+++ app/src/main/java/com/github/beatrizgomees/weatherapp/activitys/MainActivity.kt
@@ -14,11 +14,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.github.beatrizgomees.weatherapp.activitys.ui.theme.WeatherAppTheme
 import com.github.beatrizgomees.weatherapp.model.MainViewModel
+import com.github.beatrizgomees.weatherapp.ui.CityDialog
 import com.github.beatrizgomees.weatherapp.ui.nav.BottomNavBar
 import com.github.beatrizgomees.weatherapp.ui.nav.MainNavHost
 import androidx.compose.material3.FloatingActionButton as FloatingActionButton1
@@ -30,10 +35,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val mainViewModel: MainViewModel = viewModel()
-
+            var showDialog by remember { mutableStateOf(false) }
             // Passar o ViewModel para o MainNavHost
             MainNavHost(navController, mainViewModel)
             WeatherAppTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()){
+                            mainViewModel.add(city)
+                        }
+                        showDialog = false
+                    })
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -51,7 +64,9 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController)
                     },
                     floatingActionButton = {
-                        FloatingActionButton1(onClick = { }) {
+                        FloatingActionButton1(onClick = {
+                            showDialog = true
+                        }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
