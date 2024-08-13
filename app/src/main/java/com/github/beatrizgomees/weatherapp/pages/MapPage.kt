2f598b49e0763e4a2a06838fc21e70fe79bc4509
beatrizgomees <beatrizgomesxx@gmail.com>
2024-08-13 12:28:03 -0300

    Prática 04 - Parte 04 - Trabalhando com a localização do usuário

--- app/src/main/java/com/github/beatrizgomees/weatherapp/pages/MapPage.kt
+++ app/src/main/java/com/github/beatrizgomees/weatherapp/pages/MapPage.kt
@@ -1,7 +1,9 @@
 package com.github.beatrizgomees.weatherapp.pages
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +15,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,12 +27,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.github.beatrizgomees.weatherapp.R
 import com.github.beatrizgomees.weatherapp.model.MainViewModel
 import com.github.beatrizgomees.weatherapp.pages.ui.theme.WeatherAppTheme
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -39,7 +47,18 @@ fun MapPage(modifier: Modifier = Modifier, viewModel: MainViewModel, context: Co
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
     val camPosState = rememberCameraPositionState ()
+
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     GoogleMap (modifier = Modifier.fillMaxSize(),
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true),
         cameraPositionState = camPosState,
         onMapClick = { viewModel.add("Nova cidade", location = it) }) {
         Marker(
